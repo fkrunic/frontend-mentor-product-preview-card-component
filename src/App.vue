af1df62694c8e1e56db352db09c369156d535dbf
Filip Krunic <fkrunic9@gmail.com>
2023-08-31 23:39:41 -0700

    desktop design

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
 
 <template>
   <div class="flex justify-center items-center min-h-screen">
-    <div class="w-96 bg-white rounded-2xl shadow-lg">
-      <img class="rounded-tl-2xl rounded-tr-2xl" src="./assets/img/image-product-mobile.jpg">
+    <div class="flex flex-col desktop:flex-row w-96 desktop:w-[40rem] bg-white rounded-2xl shadow-lg">
+      <img class="rounded-tl-2xl rounded-tr-2xl desktop:hidden" src="./assets/img/image-product-mobile.jpg">
+      <img class="hidden desktop:block h-[30rem] rounded-tl-2xl rounded-bl-2xl" src="./assets/img/image-product-desktop.jpg">
       <div class="px-8 flex flex-col gap-5 py-6 pb-7">
         <p class="text-dark-grayish-blue font-montserrat tracking-perfume text-xs">
           PERFUME
@@ -14,7 +15,7 @@
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creater for the House of CHANEL.
         </p>
-        <div class="flex flex-row justify-start items-center gap-6">
+        <div class="flex flex-row justify-start items-center gap-6 desktop:py-3">
           <p class="text-dark-cyan text-4xl font-bold font-fraunces">$149.99</p>
           <p class="font-montserrat text-dark-grayish-blue line-through text-sm">$169.99</p>
         </div>
