bd5f3ad933ffe35be7b4d0771ed1894a9cbb249e
Filip Krunic <fkrunic9@gmail.com>
2023-08-31 22:21:15 -0700

    first commit and design setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    colors: {
+      'dark-cyan': 'hsl(158, 36%, 37%)',
+      'cream': 'hsl(30, 38%, 92%)',
+      'very-dark-blue': 'hsl(212, 21%, 14%)',
+      'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+      white: 'hsl(0, 0%, 100%)'
+    },
+    screens: {
+      desktop: '1440px'
+    },
+    fontFamily: {
+      fraunces: 'Fraunces, sans-serif',
+      montserrat: 'Montserrat, sans-serif'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
