e3b7a04a61d179ef808cd5a6ac9f8f28e077c42b
Filip Krunic <fkrunic9@gmail.com>
2023-08-31 23:44:36 -0700

    added trailing slash to base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend-mentor-product-preview-card-component',
+  base: '/frontend-mentor-product-preview-card-component/',
   plugins: [vue()],
 })
